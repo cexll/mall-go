bde121448781039f02c86dff1a857ee9d8e67e00
cexll <evanxian9@gmail.com>
2022-08-02 14:05:13 +0800

    update client_test

--- common/grpc/client_test.go
+++ common/grpc/client_test.go
@@ -9,14 +9,14 @@ import (
 )
 
 func TestNewClient(t *testing.T) {
-	cli, err := NewClient(":10002", func(options *ClientOptions) {})
+	userConn, err := NewClient(UserGRPCAddr, func(options *ClientOptions) {})
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	ucli := pb.NewUserClient(cli.Conn())
-	uresp, err := ucli.Register(context.Background(), &pb.RegisterRequest{
+	userCli := pb.NewUserClient(userConn.Conn())
+	registerResp, err := userCli.Register(context.Background(), &pb.RegisterRequest{
 		Mobile:   "19992399999",
 		Nickname: "cexll",
 		Password: "123123",
@@ -25,19 +25,27 @@ func TestNewClient(t *testing.T) {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println(uresp)
+	fmt.Println(registerResp)
 
-	cli, err = NewClient(":10012", func(options *ClientOptions) {})
+	balanceConn, err := NewClient(BalanceGRPCAddr, func(options *ClientOptions) {})
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	bcli := bpb.NewBalanceClient(cli.Conn())
-	bresp, err := bcli.GetBalance(context.Background(), &bpb.GetBalanceRequest{Type: 1, UserId: 2})
+	balanceCli := bpb.NewBalanceClient(balanceConn.Conn())
+	getBalanceResp, err := balanceCli.GetBalance(context.Background(), &bpb.GetBalanceRequest{Type: 1, UserId: 2})
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println(bresp)
+	fmt.Println(getBalanceResp)
 
+	getUserResp, err := userCli.GetUser(context.Background(), &pb.GetUserRequest{
+		Id: 1,
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(getUserResp)
 }
---
+++ common/grpc/server_addr.go
@@ -0,0 +1,6 @@
+package grpc
+
+var (
+	UserGRPCAddr    = ":10002"
+	BalanceGRPCAddr = ":10012"
+)
