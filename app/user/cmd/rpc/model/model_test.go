55ddb2fd1d94ac27d4734b43410781a086ac6ac3
cexll <evanxian9@gmail.com>
2022-08-08 18:21:49 +0800

    style model_test

--- app/user/cmd/rpc/model/model_test.go
+++ app/user/cmd/rpc/model/model_test.go
@@ -43,30 +43,3 @@ func TestMallUser_FindByWhere(t *testing.T) {
 	}
 	fmt.Println(user)
 }
-
-func TestMallUser_UpdateByWhere(t *testing.T) {
-	user := MallUser{}
-
-	// where update
-	rows, err := user.UpdateByWhere([]string{
-		"id = ?",
-	}, []any{
-		1,
-	}, []string{
-		"nickname = ?",
-		"mobile = ?",
-	}, []any{
-		"张大仙",
-		"19992399999",
-	})
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(rows)
-	// 2022/08/05 17:40:41 SQL Time: 1.5781ms  Sql: UPDATE `mall_user` SET nickname = ?, mobile = ? WHERE id = ?;  Args: [张大仙 1999239999   9 %!s(int=1)]
-}
-
-func TestMallUser_DeleteByWhere(t *testing.T) {
-
-}
